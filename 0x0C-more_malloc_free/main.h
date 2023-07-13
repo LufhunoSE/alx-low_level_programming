d48e973a6b53a28036e201f151bb3ebf5d00aa98
LufhunoSE <lufhunotlotleng@gmail.com>
2023-07-13 14:17:18 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,22 +1,15 @@
+Main.h codes
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
-
-int *array_range(int min, int max);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-void errors(void);
-int _putchar(character c);
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
-char *string_nconcat(character *s1, character *s2, unsigned int n);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-void errors(void);
-int _strlen(character *s);
-int is_digit(character *s);
 
 #endif
 
