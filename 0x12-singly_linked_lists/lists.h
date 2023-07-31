d20fb75a61a51b95f9b428200652f91572e78608
LufhunoSE <lufhunotlotleng@gmail.com>
2023-07-31 11:22:50 -0700

    task 1

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,4 +1,4 @@
-#ifdef LISTS_H
+#ifndef LISTS_H
 #define LISTS_H
 
 /**
@@ -11,11 +11,10 @@
  */
 typedef struct list_s
 {
-  char *str;
-  unsigned int len;
-  struct list_s *next;
-  list_t;
-}
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
@@ -23,4 +22,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif
+#endif  /* LIST_H */
